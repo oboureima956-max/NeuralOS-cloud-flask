063cc80b9e74f4bb86cc6be718f88fcdfaf2ecb5
Boureima Ouedraogo <oboureima956@gmail.com>
2026-04-02 18:41:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask, render_template, jsonify
-import random
+ from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/eeg')
-def eeg():
-    data = [random.randint(0, 100) for _ in range(10)]
-    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
